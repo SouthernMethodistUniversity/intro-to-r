3ae02dbd186d7e36e6ca44c08960d318a67fd678
Mateo Langston Smith <mlangstonsmith@smu.edu>
2025-01-27 09:58:00 -0600

    Basics Code File

--- code/01_Basics.R
+++ code/01_Basics.R
@@ -1,3 +1,19 @@
-# 2.3.2 Console
+#### 1 Basics - R Code ####
+
+#### Console ####
 
 1 + 1
+
+#### Assigning Variables ####
+
+x <- 2
+x
+
+y = 2
+y
+
+#### Comments ####
+
+# This is a comment
+z <- 3 # This is also a comment
+z
