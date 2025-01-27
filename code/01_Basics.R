48ab94c57363f351ba5de17012dbec01265c1c6a
mateolangston <mateolangston@users.noreply.github.com>
2025-01-27 16:40:25 +0000

    deploy: 9174fb130f766e22d6d7611ac466023c343c71c6

---
+++ code/01_Basics.R
@@ -0,0 +1,19 @@
+#### 1 Basics - R Code ####
+
+#### Console ####
+
+1 + 1
+
+#### Assigning Variables ####
+
+x <- 2
+x
+
+y = 2
+y
+
+#### Comments ####
+
+# This is a comment
+z <- 3 # This is also a comment
+z
